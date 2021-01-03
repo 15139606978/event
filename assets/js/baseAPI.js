5ada2c7f6091022af227af993d4170a5c1771c96
wangfang <1239650408@qq.com>
2021-01-04 00:43:28 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,26 @@
 //在这个函数中，可以拿到我们给Ajax提供的配置对象
 
 $.ajaxPrefilter(function (options) {
-    
-    options.url = 'http://ajax.frontend.itheima.net'+options.url
-    console.log(options.url)
+
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    //统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载complete回调函数
+    options.complete = function (res) {
+        // console.log('执行了complete回调:');
+        // console.log(res);
+        //在complete 回调函数中，可以调用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.强制清空token
+            localStorage.removeItem('token')
+            //2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
